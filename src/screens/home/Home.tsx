9b6bbade436461fca1aebc19142eab7d283a6db2
5CG9386HF5\Onizea <genefernandz@gmail.com>
2025-07-10 14:31:58 +0200

    Agregar codico semantico de ionic, se adapta a la pantalla completa, pero todavía se tiene que mejorar la interacción del mapa cuando no se est´s usando

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -8,20 +8,29 @@ const Home: React.FC = () => {
 
   return (
     <IonPage>
-      <IonContent  className="ion-padding">
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          {/* <IonToolbar> */}
-            {/* <IonTitle size="large">Tab 1</IonTitle> */}
-          {/* </IonToolbar> */}
-        </IonHeader>
-        <MapboxMapHome />
-      </IonContent>
+    
+      <IonContent className='main-container'>
+
+          <IonHeader className='header-container'>
+              <IonToolbar>
+                  <IonTitle>Perpicto</IonTitle>
+              </IonToolbar>
+          </IonHeader>
+
+
+          {/* Aqui agregas <IonContent></IonContent> como si fuesen div para buscador y filtros  */}
+
+
+          <IonContent className='mapbox-container'>
+            <MapboxMapHome />
+          </IonContent>
+
+
+          <IonContent className='result-search-container'>
+          {/* Aqui condicionas el <IonContent></IonContent> para que te genere div para la lista de palabras encontradas
+          al realizar la busqueda (item)*/}
+          </IonContent>
+
       </IonContent>
     </IonPage>
   );
