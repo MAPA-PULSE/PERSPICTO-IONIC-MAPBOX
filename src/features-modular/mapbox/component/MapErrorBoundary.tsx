bb2b6f112ee6f2ae33f16c10858046de6ef49085
5CG013090L\Onizea <lorena.odena@hotmail.com>
2025-07-21 16:35:41 +0200

    he comentado MapErrorBoundary.tsx

--- src/features-modular/mapbox/component/MapErrorBoundary.tsx
+++ src/features-modular/mapbox/component/MapErrorBoundary.tsx
@@ -1,17 +1,17 @@
-//PERSPICTO-IONIC-MAPBOX\src\features-modular\mapbox\component\MapErrorBoundary.tsx
-import { MapErrorBoundary } from './MapErrorBoundary';
-import MapboxMap from './MapboxMap';
+// //PERSPICTO-IONIC-MAPBOX\src\features-modular\mapbox\component\MapErrorBoundary.tsx
+// //import { MapErrorBoundary } from './MapErrorBoundary';
+// import MapboxMap from './MapboxMap';
 
-const MapView = () => {
-  const handleMapLoad = (map: mapboxgl.Map) => {
-    // cualquier lógica adicional al cargar
-  };
+// const MapView = () => {
+//   const handleMapLoad = (map: mapboxgl.Map) => {
+//     // cualquier lógica adicional al cargar
+//   };
 
-  return (
-    <MapErrorBoundary>
-      <MapboxMap onMapLoad={handleMapLoad} />
-    </MapErrorBoundary>
-  );
-};
+//   return (
+//     <MapErrorBoundary>
+//       <MapboxMap onMapLoad={handleMapLoad} />
+//     </MapErrorBoundary>
+//   );
+// };
 
-export default MapView;
+// export default MapView;
