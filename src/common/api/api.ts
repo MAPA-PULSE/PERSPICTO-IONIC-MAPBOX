4eb6af91e7c26606b30f62c252a9ec12da8db64a
5CG9386HF5\Onizea <genefernandz@gmail.com>
2025-07-28 16:20:38 +0200

    Crear token de acceso para el usuario y avisar cuando haya un cambio en las apis.

--- src/common/api/api.ts
+++ src/common/api/api.ts
@@ -1,6 +1,6 @@
 //const API_BASE_URL = "https://backend-perspicto-t413.onrender.com";
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "http://0.0.0.0:8000";
 
 export default API_BASE_URL;
 
